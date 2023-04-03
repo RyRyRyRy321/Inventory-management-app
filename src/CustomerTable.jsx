60ae4f08a2407a245cf5a193aa578907c4374911
RyRyRyRy321 <hryanjames100@gmail.com>
2023-04-03 11:11:39 +0800

    Modify Update customer modal

--- src/CustomerTable.jsx
+++ src/CustomerTable.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import {Modal, Button, Form, Container, Row, Col, Stack} from 'react-bootstrap'
+import {Modal, Button, Form, Container, Row, Col, Stack, InputGroup} from 'react-bootstrap'
 
 function CustomerTable({customerData}){
 
@@ -84,94 +84,80 @@ function UpdateCustomerModal({show, eventClose, targetData}){
   });
 
   return (
-    <Modal show={show} onHide={eventClose} size='xl'>
+    <Modal show={show} onHide={eventClose} size='lg'>
 
       <Modal.Header closeButton>
         <Modal.Title>Update Customer</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-
+      <Form>
         <Container>
           <Row className='mb-2'>
             <Col col={6}>
               <Form.Label>Product Name</Form.Label>
               <Form.Control type = 'text'></Form.Control>
             </Col>
-
             <Col col={6}>
               <Form.Label>Category</Form.Label>
               <Form.Select aria-label="Default select example">
                 <option value="test_1">option 1</option>
                 <option value="test_2">option 2</option>
                 <option value="test_3">option 3</option>
-
-
               </Form.Select>
             </Col>
-
           </Row>
-
           <Row className='mb-2'>
             <Col>
               <Form.Label>Product description</Form.Label>
-              <Form.Control as ="textarea" rows={5} placeholder='Maximum of 300 characters'></Form.Control>            
+              <Form.Control as ="textarea" rows={5} placeholder='Maximum of 300 characters'></Form.Control>
             </Col>
           </Row>
-
           <Row className='mb-2'>
-            <Col>
+            <Col col={6}>
               <Form.Label>Quantity</Form.Label>
               <Form.Control type = 'number'></Form.Control>
             </Col>
-          </Row>
-
-          <Row className='mb-2'>
 
             <Col col={6}>
-              <Form.Label>Product price</Form.Label>
-
-              <Stack direction='horizontal' gap = {1}>
-                <Form.Label>₱</Form.Label>
-                
-                <Form.Control type = 'number'></Form.Control>
-
-              </Stack>
-            </Col>
-
-            <Col col={6}>
-              <Form.Label>Unit price</Form.Label>
-
-              <Stack direction='horizontal' gap = {1}>
-                <Form.Label>₱</Form.Label>
-                
-                <Form.Control type = 'number'></Form.Control>
-
-              </Stack>
-            </Col>
-
-          </Row>
-
-          <Row className='mb-2'>
-
-            <Col col={2}>
-              <Form.Label>Discount</Form.Label>
-              <Form.Control type='number'></Form.Control>
-            </Col>
-
-            <Col col={2}>
               <Form.Label>Stock alert</Form.Label>
               <Form.Control type='number'></Form.Control>
             </Col>
 
+          </Row>
+          <Row className='mb-2'>
+            <Col col={6}>
+              <Form.Label>Product price</Form.Label>
+
+              <InputGroup>
+                <InputGroup.Text>₱</InputGroup.Text>
+                <Form.Control type = 'number'></Form.Control>
+              </InputGroup>
+      
+            </Col>
+            <Col col={6}>
+              <Form.Label>Unit price</Form.Label>
+
+              <InputGroup>
+                <InputGroup.Text>₱</InputGroup.Text>
+                <Form.Control type = 'number'></Form.Control>
+              </InputGroup>
+              
+            </Col>
+          </Row>
+          <Row className='mb-2'>
+            <Col col={1}>
+              <Form.Label>Discount</Form.Label>
+              <Form.Control type='number'></Form.Control>
+            </Col>
+            
             <Col>
               <Form.Label>Product Image</Form.Label>
               <Form.Control type='file'></Form.Control>
             </Col>
-
           </Row>
-
         </Container>
+      </Form>
 
         {/* <Form.Label>Name</Form.Label>
         <Form.Control name='name' type = "text" placeholder={targetData.name} value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} ></Form.Control>
