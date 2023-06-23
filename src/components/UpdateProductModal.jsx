f112ccf6f98c49f082ef3cf75ffab3b0ac3c7bd2
RyRyRyRy321 <hryanjames100@gmail.com>
2023-06-23 18:31:02 +0800

    Refactor Update Product Modal

--- src/components/UpdateProductModal.jsx
+++ src/components/UpdateProductModal.jsx
@@ -1,134 +1,118 @@
 import { useEffect, useState} from 'react';
 import {Modal, Button, Form, Container, Row, Col, Stack, InputGroup} from 'react-bootstrap'
-import axios from 'axios';
 import ProductRepository from '../scripts/ProductRepository';
+import {useFormik} from 'formik';
+import * as Yup from 'yup'; 
+import ProductForm from './ProductForm';
+
+
+const categoryData = [
+
+  {categoryId: 1, categoryName: 'Paper flower', categoryValue: 'PAPER_FLOWER'},
+  {categoryId: 2, categoryName: 'Balloon', categoryValue: 'BALLONS'},
+  {categoryId: 3, categoryName: 'Glassware', categoryValue: 'GLASSWARE'},
+
+];
 
 function UpdateCustomerModal({show, eventClose, targetData, rerenderEvent}){
 
-  const [productFormData, setProductFormData] = useState({});
-  useEffect(() => {setProductFormData(targetData)}, [targetData])
+  const original_productSchema = Yup.object().shape({
+    productName: Yup.string().min(8, "Product name is too short").max(20, "Product name is too long").required("Product name is required"),
+    category: Yup.string().required("Select a category"),
+    productDesc: Yup.string().max(300, "Maximum characters of 300").required("Product description is required"),
+    quantity: Yup.number().test('maxDigits','Quantity can have a maximum of 9 digits',value => value.toString().length <= 9
+    ).required('Quantity is required').moreThan(-1,"Quantity cannot have negative value"),
+    stockAlertQuantity: Yup.number().test('maxDigits','Stock alert quantity can have a maximum of 9 digits',value => value.toString().length <= 9
+    ).required('Stock alert quantity is required').moreThan(-1,"Stock alert quantity cannot have negative value"),
+    productPrice: Yup.number().test('maxDigits','Product price can have a maximum of 9 digits',value => value.toString().length <= 9
+    ).required('Product price is required').moreThan(-1,"Product price cannot have negative value"),
+    unitPrice: Yup.number().test('maxDigits','Unit price can have a maximum of 9 digits',value => value.toString().length <= 9
+    ).required('Unit price is required').moreThan(-1,"Unit price cannot have negative value"),
+    discount: Yup.number().min(0,"Yes").max(1,"No")
+  })
+ 
+  const emptyProductForm = {
+    productName:'',
+    category:'NONE',
+    productDesc:'',
+    quantity:'',
+    stockAlertQuantity:'',
+    productPrice:'',
+    unitPrice:'',
+    discount:'',
+  }
 
+  useEffect(
+    () => {
+      formik.setValues(emptyProductForm)
+    },
+    []
+  )
 
-  const categoryData = [
+  const handleFormClose = (values) => {
+    formik.resetForm();
+    formik.setErrors({});
+    eventClose();
+  }
 
-    {categoryId: 1, categoryName: 'Paper flower', categoryValue: 'PAPER_FLOWER'},
-    {categoryId: 2, categoryName: 'Balloon', categoryValue: 'BALLONS'},
-    {categoryId: 3, categoryName: 'Glassware', categoryValue: 'GLASSWARE'},
-
-  ];
-
-
-  const handleChange = (e) => {
-    setProductFormData({ ...productFormData, [e.target.name]: e.target.value });
-  };
-
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleFormSubmit = async (values) => {
 
     const productId = targetData.productId;
-    
+
     try {
-      const response = await ProductRepository.updateProduct(productId, productFormData)   
-      setProductFormData(targetData);
-      rerenderEvent();
+
+      console.log(values);
+      // const response = await ProductRepository.updateProduct(productId, values);
+      formik.resetForm();
+      formik.setErrors({});
       eventClose();
+      rerenderEvent();
     } catch (error) {
       console.error(error);
     }
 
+    
+  }
 
+  const formik = useFormik({
+    initialValues: targetData,
+    enableReinitialize: true,
+    validationSchema: original_productSchema,
+    onSubmit: handleFormSubmit
+  })
 
-  };
+  
 
 
     return (
-      <Modal show={show} onHide={eventClose} size='lg'>
+      <Modal show={show} onHide={handleFormClose} size='lg'>
   
         <Modal.Header closeButton>
           <Modal.Title>Update Product</Modal.Title>
         </Modal.Header>
-  
-        <Modal.Body>
-        <Form>
-          <Container>
-            <Row className='mb-2'>
-              <Col col={6}>
-                <Form.Label>Product Name</Form.Label>
-                <Form.Control name = 'productName' defaultValue={targetData.productName} type = 'text' onChange={handleChange}></Form.Control>
-              </Col>
-              <Col col={6}>
-                <Form.Label>Category</Form.Label>
-                <Form.Select name = 'category' defaultValue = {targetData.category} aria-label="Default select example" onChange={handleChange}>
-                  {
-                    categoryData.map((categoryItem) => renderOptions(categoryItem))
-                  }
-                </Form.Select>
-              </Col>
-            </Row>
+
+        <Form noValidate onSubmit={formik.handleSubmit}>
+
+          <Modal.Body>
+
+          <ProductForm values={formik.values} errors={formik.errors} handleChange={formik.handleChange} touched={formik.touched} categoryData={categoryData} ></ProductForm>
+
+
+
             
-            <Row className='mb-2'>
-              <Col>
-                <Form.Label>Product description</Form.Label>
-                <Form.Control as ="textarea" name = 'productDesc' defaultValue={targetData.productDesc} rows={5} placeholder='Maximum of 300 characters' onChange={handleChange}></Form.Control>
-              </Col>
-            </Row>
-            <Row className='mb-2'>
-              <Col col={6}>
-                <Form.Label>Quantity</Form.Label>
-                <Form.Control type = 'number' name = 'quantity' defaultValue={targetData.quantity} onChange={handleChange}></Form.Control>
-              </Col>
+          
+    
+          </Modal.Body>
   
-              <Col col={6}>
-                <Form.Label>Stock alert</Form.Label>
-                <Form.Control type='number' name = 'stockAlertQuantity' defaultValue={targetData.stockAlertQuantity} onChange={handleChange}></Form.Control>
-              </Col>
-  
-            </Row>
-            <Row className='mb-2'>
-              <Col col={6}>
-                <Form.Label>Product price</Form.Label>
-  
-                <InputGroup>
-                  <InputGroup.Text>₱</InputGroup.Text>
-                  <Form.Control type = 'number' name = 'productPrice' defaultValue={targetData.productPrice} onChange={handleChange}></Form.Control>
-                </InputGroup>
-        
-              </Col>
-              <Col col={6}>
-                <Form.Label>Unit price</Form.Label>
-  
-                <InputGroup>
-                  <InputGroup.Text>₱</InputGroup.Text>
-                  <Form.Control type = 'number' name = 'unitPrice' defaultValue={targetData.unitPrice} onChange={handleChange}></Form.Control>
-                </InputGroup>
-                
-              </Col>
-            </Row>
-            <Row className='mb-2'>
-              <Col col={1}>
-                <Form.Label>Discount</Form.Label>
-                <Form.Control type='number' name = 'discount' defaultValue={targetData.discount} onChange={handleChange}></Form.Control>
-              </Col>
-              
-              <Col>
-                <Form.Label>Product Image</Form.Label>
-                <Form.Control type='file'></Form.Control>
-              </Col>
-            </Row>
-          </Container>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={eventClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={formik.handleSubmit}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
         </Form>
-  
-        </Modal.Body>
-  
-        <Modal.Footer>
-          <Button variant="secondary" onClick={eventClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleSubmit}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
       </Modal>
     );
   
